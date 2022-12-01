7a9e5daa3451fc17eb0d8e034ddd900fa2e7b209
DANIZIN <98532797+DANIZIN23@users.noreply.github.com>
2022-11-30 21:21:43 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -77,7 +77,7 @@ class Main extends Sprite
 		// the reason for this is we're going to be handling our own cache smartly
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
-		#if !mobile
+		
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
 		Lib.current.stage.align = "tl";
@@ -85,7 +85,7 @@ class Main extends Sprite
 		if(fpsVar != null) {
 			fpsVar.visible = ClientPrefs.showFPS;
 		}
-		#end
+		
 
 		#if html5
 		FlxG.autoPause = false;
